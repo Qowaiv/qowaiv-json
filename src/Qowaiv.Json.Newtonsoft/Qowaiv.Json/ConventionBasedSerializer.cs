d2ea0f45d1776916dcc8f92db1075b0cb01297c5
Corniel Nobel <mail@corniel.nl>
2019-12-11 10:00:47 +0100

    Small format tweak.

--- src/Qowaiv.Json.Newtonsoft/Qowaiv.Json/ConventionBasedSerializer.cs
+++ src/Qowaiv.Json.Newtonsoft/Qowaiv.Json/ConventionBasedSerializer.cs
@@ -26,12 +26,12 @@ namespace Qowaiv.Json
                 return reader.TokenType switch
                 {
                     // Empty value for null-ables.
-                    JsonToken.Null => isNullable ? null : Activator.CreateInstance(objectType),
-                    JsonToken.String => fromJsonString((string)reader.Value),
-                    JsonToken.Float => fromJsonDouble((double)reader.Value),
-                    JsonToken.Integer => fromJsonLong((long)reader.Value),
-                    JsonToken.Boolean => fromJsonBool(true.Equals(reader.Value)),
-                    JsonToken.Date => fromJsonString(((DateTime)reader.Value).ToString(CultureInfo.InvariantCulture)),
+                    JsonToken.Null /*   */ => isNullable ? null : Activator.CreateInstance(objectType),
+                    JsonToken.String /* */ => fromJsonString((string)reader.Value),
+                    JsonToken.Float /*  */ => fromJsonDouble((double)reader.Value),
+                    JsonToken.Integer /**/ => fromJsonLong((long)reader.Value),
+                    JsonToken.Boolean /**/ => fromJsonBool(true.Equals(reader.Value)),
+                    JsonToken.Date /*   */ => fromJsonString(((DateTime)reader.Value).ToString(CultureInfo.InvariantCulture)),
 
                     // Other scenario's are not supported.
                     _ => throw new JsonSerializationException($"Unexpected token parsing {objectType.FullName}. {reader.TokenType} is not supported."),
@@ -56,7 +56,6 @@ namespace Qowaiv.Json
             Guard.NotNull(writer, nameof(writer));
 
             var json = (TSvo)value;
-
             var node = ToJson(json);
 
             if (node is null)
